265ceed79ae258d0d263b405444d135d3ada41ae
blase147 <solarmails2@gmail.com>
2023-03-07 10:14:12 +0100

    Add model associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  has_many :groups, class_name: 'Group', foreign_key: 'user_id'
+  has_many :expenses, class_name: 'Expense', foreign_key: 'user_id'
+
+  validates :name, presence: true, length: { in: 2..300 }
+
+  def admin?
+    role == 'admin'
+  end
 end
