321dd3c133edcffdb769141a562cfb5c9d7e731b
blase147 <solarmails2@gmail.com>
2023-03-11 12:10:36 +0100

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -106,4 +106,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.hosts << "budget-app-5uzz.onrender.com"
 end
