ceb701ec259761ce74d3ea9a96524e5ea2c206f2
blase147 <solarmails2@gmail.com>
2023-03-11 16:28:16 +0100

    Fix nameError

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'rubocop', '>= 1.0', '< 2.0'
 gem 'devise'
 
 # rails controller spec
-# gem 'rails-controller-testing'
+gem 'rails-controller-testing'
 
 # authorization
 gem 'cancancan'
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
 
   has_many :groups, class_name: 'Group', foreign_key: 'user_id'
   has_many :expenses, class_name: 'Expense', foreign_key: 'user_id'
 
   validates :name, presence: true, length: { in: 2..300 }
+  validates :email, presence: true
+  validates :password, presence: true
 
   def admin?
     role == 'admin'
