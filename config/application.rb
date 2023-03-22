329f489bba3b0435fd39093b106eb6affde3bca1
blase147 <solarmails2@gmail.com>
2023-03-22 09:58:35 +0100

    Remove require bullet from application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
+require 'bullet'
 require_relative "boot"
 
 require "rails/all"
-
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
